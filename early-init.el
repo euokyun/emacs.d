916a5b45364c8ea5919224d79d42ecfbf415e612
euokyun <euokyun@gmail.com>
2021-12-05 23:35:05 +0900

    CLEAN: early-init ADD: path, writeroom

--- early-init.el
+++ early-init.el
@@ -18,10 +18,6 @@
 ;; package initialization occurs before `user-init-file' is loaded, but after the `early-init-file'.
 (setq package-enable-at-startup nil)
 
-
-;; (setq default-file-name-handler-alist file-name-handler-alist
-;;       file-name-handler-alist nil)
-
 ;; ----------------------------------------------------------------
 ;; use straight.el as package manager.
 ;; will cache autoloads for all package. I/O workload will be O(n) -> O(1).
@@ -52,9 +48,10 @@
 
 ;; https://github.com/purcell/exec-path-from-shell
 (use-package exec-path-from-shell
-  ;; :if (memq window-system '(mac ns))    ; while in early-init, this not works. maybe window-system is not recognized.
-  ;; :custom
+  :custom
   ;; (exec-path-from-shell-arguments nil)
+  ;; (exec-path-from-shell-debug t)
+  (exec-path-from-shell-variables '("PATH" "MANPATH" "SHELL" "JAVA_HOME"))
   :config
   (exec-path-from-shell-initialize))
 
@@ -69,25 +66,6 @@
   (require 'recentf) ;; recent files에 var, etc 제외
   (add-to-list 'recentf-exclude no-littering-var-directory)
   (add-to-list 'recentf-exclude no-littering-etc-directory))
-
-
-;; Disable most GUI widgets early on
-;; (setq default-frame-alist
-;;       '((font . "JetBrains Mono")
-;;         (horizontal-scroll-bars . nil)
-;;         (vertical-scroll-bars . nil)
-;;         (ns-appearance . dark)          ; hide titlebar
-;;         (ns-transparent-titlebar . t)   ; hide titlebar
-;;         (scroll-bar-mode -1)
-;;         (tool-bar-mode -1)
-;;         (menu-bar-lines . 0)
-;;         (tool-bar-lines . 0)
-;;         (internal-border-width . 0)
-;;         (frame-title-format "\n")
-;;         ;; (height . 50)
-;;         ;; (width . 95)
-;;         (ns-use-proxy-icon nil)
-;;         ))
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
@@ -108,10 +86,5 @@
 (set-default-coding-systems 'utf-8)
 (set-language-environment "UTF-8")
 (setq buffer-file-coding-system 'utf-8)
-;; (add-hook 'emacs-startup-hook
-;;           (lambda (&rest _)
-;;             (setq file-name-handler-alist default-file-name-handler-alist)
-;;             ;; delete no longer necessary startup variable
-;;             (makunbound 'default-file-name-handler-alist)))
 
 ;;; early-init.el ends here
