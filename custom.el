a61fb392589c9955fb5bc0f5f4eac0651d3ffed5
euokyun <euokyun@gmail.com>
2022-01-15 00:58:04 +0900

    fix: corfu-doc docstring

--- custom.el
+++ custom.el
@@ -7,10 +7,7 @@
    '("e2fe2a8813516bbb6f55caaa52d52c60d751de5d7b52a49dda9a21d0e6a361ac" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "701c25b42357bc2675bf934942a01332cf0c37ad00b3f66eac225970244d59b5" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))
  '(lsp-semantic-tokens-enable t nil nil "Customized with use-package lsp-mode")
  '(org-preview-latex-default-process 'dvisvgm nil nil "Customized with use-package org")
- '(safe-local-variable-values
-   '((eval when
-           (fboundp 'rainbow-mode)
-           (rainbow-mode 1)))))
+ '(safe-local-variable-values '((eval when (fboundp 'rainbow-mode) (rainbow-mode 1)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
